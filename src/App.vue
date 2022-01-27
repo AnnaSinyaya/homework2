7fedb8748352d38b381145dea365dd3f1e21e973
AnnaSinyaya <97228707+AnnaSinyaya@users.noreply.github.com>
2022-01-27 09:49:21 +0300

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <bookMainWrapper/>
+  </div>
+</template>
+
+<script>
+import bookMainWrapper from "./components/book-main-wrapper.vue"
+export default {
+  name: 'App',
+  components: {
+   bookMainWrapper
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/book-main-wrapper.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="book-main-wrapper">
+        <p>{{title}}</p>
+        <book-catalog></book-catalog>
+        <book-create></book-create>
+        <book-search></book-search>
+    </div>
+</template>
+
+<script>
+import bookCatalog from './book-catalog.vue'
+import bookCreate from './book-create.vue'
+import bookSearch from './book-search.vue'
+
+export default {
+    name:'book-main-wrapper',
+    components: {
+        bookCatalog,
+        bookCreate,
+        bookSearch,
+    },
+    props: {},
+    data(){
+        return {
+            title: 'Домашнее задание №2'
+        }
+    },
+    computed: {},
+    methods: {},
+    watch: {},
+    mounted(){
+        console.log('mounted!');
+    }
+}
+</script>
+
+<style>
+.book-main-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 900px;
+    margin: 0 auto;
+}
+</style>
